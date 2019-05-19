fe04e908963c91fdcef280a1891b887349e26ef5
Froglegg <hayescrowley@gmail.com>
2019-05-19 12:53:53 -0400

    yep

---
+++ fanpage.php
@@ -0,0 +1,38 @@
+<!doctype html>
+<html lang="en-US">
+<head>
+<?php
+include 'head.php';
+?>
+    <title>Froglegg's favorite band</title>
+</head>
+
+<body>
+<header class='masthead'>
+        <p class='masthead-intro'>This page is about the greatest band of all time....</p>
+        <h1 class='masthead-heading'>CREEDENCE CLEARWATER REVIVAL (CCR)</h1>
+    </header>
+    <section class="details">
+      <h1>Introduction</h1>
+      <p>I was born in the swamp.</p>
+      <h1>Where I'm From</h1>
+      <p>I'm from the swamp.</p>
+      <h1>What are your favorite hobbies?</h1>
+      <p>My hobbies include music, hiking/backpacking, reading, croaking, hopping, etc.</p>
+      <h1>What's your dream job?</h1>
+      <p>Eating flies.</p>
+      <h1>Where do you live?</h1>
+      <p>I've never left the swamp, I'm a swamp boi through and through.</p>
+      <h1>Why do you want to be a web developer?</h1>
+      <p>Dunno, hard being a frog these days, can't just eat flies all the time, gotta make a living somehow...</p>
+    </section>
+
+    <footer id="footer">
+        <ul class="social">
+          <li><a href="http://www.github.com/froglegg">Github</a></li>
+        </ul>
+    </footer>
+</body>
+
+
+</html>
---
+++ head.php
@@ -0,0 +1,28 @@
+<meta charset="utf-8"/>
+<html lang="en-US">
+
+<!--this be your meta section; 'viewport' is for responsive design, don't set overly large widths!-->
+
+<meta name="keywords" content="Froglegg!">
+<meta name="description" content="All about froglegg!!!">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+<!-- this be your favicon-->
+
+
+<!--these be your styles-->
+
+<link rel="stylesheet" type="text/css" href= "css/style.css?<?php echo time(); ?>" media="screen">
+
+<link rel="stylesheet" media="screen and (max-width: 768px)" href="css/mobile-styles.css?<?php echo time(); ?>" media="screen">
+
+
+<!--these be your google fonts-->
+<link href="https://fonts.googleapis.com/css?family=Karla|Montserrat|Open+Sans" rel="stylesheet">
+
+
+<!-- Global site tag (gtag.js) - Google Analytics -->
+
+
+<!-- jquery CDN-->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
